43b3586a6cd6663210ca738b5b8cc7659fe1c7c1
Suzu_Yoshi_tree <acver.rainbox@gmail.com>
2020-10-19 20:15:18 +0900

    向きを修正中

--- Assets/mutant/MutantCamera.cs
+++ Assets/mutant/MutantCamera.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MutantCamera : MonoBehaviour
 {
     [SerializeField, Range(0, 90), TooltipAttribute("カメラの角度を設定します")]
     private int CameraAngle = 0; //カメラの角度
 
-    [SerializeField,Range(0.1f,10.0f), TooltipAttribute("プレイヤーとカメラの距離を設定します")]
+    [SerializeField, Range(0.1f, 10.0f), TooltipAttribute("プレイヤーとカメラの距離を設定します")]
     private float distance = 3.5f; //プレイヤーとカメラの距離
 
     [SerializeField] private Transform player;  // 注視対象プレイヤー
@@ -20,7 +21,7 @@ public class MutantCamera : MonoBehaviour
     void Start()
     {
         //回転の初期化
-        vRotation = Quaternion.Euler(CameraAngle, 0, 0);
+        vRotation = Quaternion.identity;
         hRotation = Quaternion.identity;
 
         //カメラ位置の初期化(後ろに少し引く)
@@ -30,20 +31,43 @@ public class MutantCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 水平回転の更新
-        if (Input.GetMouseButton(0))
-            hRotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * turnSpeed, 0);
+        if (Input.GetMouseButton((int)MouseButton.LeftMouse))
+        {
+            //水平回転の更新
+            float mouseX = Input.GetAxis("Mouse X");
 
-        //垂直方向の更新
-        vRotation = Quaternion.Euler(CameraAngle, 0, 0);
+            //垂直回転の更新
+            float mouseY = Input.GetAxis("Mouse Y");
 
-        // カメラの回転(transform.rotation)の更新
-        // 方法1 : 垂直回転してから水平回転する合成回転とします
-        transform.rotation = hRotation * vRotation;
+            float limit = 90.0f;
+            float maxLimit = limit;
+            float minLimit = 360.0f;
+
+            //現在のオイラー角を取得
+            Vector3 localAngle = transform.localEulerAngles;
+            //マウスの移動量を加算
+            localAngle.x += mouseY;
+
+            //上向きの加算
+            if (localAngle.x > maxLimit && localAngle.x < 180)
+                localAngle.x = maxLimit;
+            
+            //下向きの加算
+            if (localAngle.x < minLimit && localAngle.x > 180)
+                localAngle.x = minLimit;
+
+            //回転処理
+            transform.localEulerAngles = localAngle;
+            
+            //Y軸回転
+            var angle = transform.eulerAngles;
+            angle.y += mouseX;
+            transform.eulerAngles = angle;
+        }
 
         // カメラの位置(transform.position)の更新
-        // player位置から距離distanceだけ手前に引いた位置を設定します(位置補正版)
-        transform.position = player.position + new Vector3(0.0f, 0.5f, 3.0f) - transform.rotation * Vector3.forward * distance;
-
+        // player位置から距離distanceだけ手前に引いた位置を設定
+        transform.position = player.position + new Vector3(0.0f, 0.5f, 2.0f) - transform.rotation * Vector3.forward * distance;
     }
+
 }
--- Assets/mutant/MutantMove.cs
+++ Assets/mutant/MutantMove.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MutantMove : MonoBehaviour
 {
-    [SerializeField, Range(0.1f, 5.0f), TooltipAttribute("プレイヤーの速度を設定します")]
-    private float moveSpeed = 1.0f;     //移動速度
+    [SerializeField, Range(0.01f, 1.0f), TooltipAttribute("プレイヤーの速度を設定します")]
+    private float moveSpeed = 0.01f;     //移動速度
 
-    private Vector3 vec;    //移動方向
-    private float applySpeed = 0.2f;  //振り向き速度
+    [SerializeField]
+    private Camera mainCam;
+
+    private Vector3 Rota;              //回転方向
+    private Vector3 velo;              //速度方向
+    private float applySpeed = 0.2f;   //振り向き速度
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -21,29 +25,43 @@ public class MutantMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        vec = Vector3.zero; //移動方向を初期化
+        //＊＊＊＊＊キャラの回転＊＊＊＊＊
 
-        if (Input.GetKey(KeyCode.W)) { vec.z += 1; }    //奥
-        if (Input.GetKey(KeyCode.S)) { vec.z -= 1; }    //手前
-        if (Input.GetKey(KeyCode.A)) { vec.x -= 1; }    //左
-        if (Input.GetKey(KeyCode.D)) { vec.x += 1; }    //右
+        Rota = Vector3.zero; //移動方向を初期化
+
+        if (Input.GetKey(KeyCode.W)) { Rota.z = +1.0f; }    //奥
+        if (Input.GetKey(KeyCode.S)) { Rota.z = -1.0f; }    //手前
+        if (Input.GetKey(KeyCode.A)) { Rota.x = -1.0f; }    //左
+        if (Input.GetKey(KeyCode.D)) { Rota.x = +1.0f; }    //右
 
         //移動ベクトルを正規化(1の長さ)にして、Spee分だけ進む
-        vec = vec.normalized * moveSpeed * Time.deltaTime;
+        Rota = Rota.normalized * moveSpeed * Time.deltaTime;
 
         //移動しているとき(magnitude：ベクトルの長さを返す)
-        if (vec.magnitude > 0)
+        if (Rota.magnitude > 0)
         {
             //プレイヤーの回転(transform.rotation)の更新
             //無回転状態のプレイヤーのZ+方向(後頭部)を、移動の反対方向(-velocity)に回す回転に段々近づける
             this.transform.rotation
                 = Quaternion.Slerp(
                     transform.rotation,
-                    Quaternion.LookRotation(vec),
+                    Quaternion.LookRotation(Rota),
                     applySpeed);
+        }
 
-            //プレイヤーの位置に足し込む
-            transform.position += vec;
+        //＊＊＊＊＊キャラの速度方向＊＊＊＊＊
+        velo = new Vector3(0.0f, 0.0f, 0.0f);
+
+        if (Input.GetKey(KeyCode.W)) { velo.z = +moveSpeed; }    //奥
+        if (Input.GetKey(KeyCode.S)) { velo.z = -moveSpeed; }    //手前
+        if (Input.GetKey(KeyCode.A)) { velo.x = -moveSpeed; }    //左
+        if (Input.GetKey(KeyCode.D)) { velo.x = +moveSpeed; }    //右
+
+        //移動しているとき(magnitude：ベクトルの長さを返す)
+        if (velo.magnitude > 0)
+        {
+            //速度を加える
+            rb.velocity = velo;
         }
     }
 }
